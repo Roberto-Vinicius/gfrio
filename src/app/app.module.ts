a4b3e83e3d33fe114780253802d15508f55bace5
ROBERTO VINICIUS DANTAS DA SILVA <roberto.dantas.706@ufrn.local>
2024-08-28 11:35:48 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SobreComponent } from './components/pages/sobre/sobre.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ServicoComponent } from './components/pages/servico/servico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SobreComponent,
+    ServicoComponent
   ],
   imports: [
     BrowserModule,
