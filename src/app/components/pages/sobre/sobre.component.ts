b66d54981d205cb9d85b43b40c1e3c17c084b2e5
roberto-vinicius <rbt.negocios@gmail.com>
2024-09-05 14:01:01 -0300

    atualização de layout

--- src/app/components/pages/sobre/sobre.component.ts
+++ src/app/components/pages/sobre/sobre.component.ts
@@ -1,10 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
-  styleUrl: './sobre.component.css'
+  styleUrls: ['./sobre.component.css'] // Corrigido de `styleUrl` para `styleUrls`
 })
 export class SobreComponent {
 
+  constructor(private http: HttpClient) {}
+
+  downloadPdf() {
+    const pdfUrl = 'assets/portifolio_gfrio.pdf'; // Caminho relativo ao PDF dentro da pasta assets
+    const pdfName = 'portifolio_gfrio.pdf'; // Nome que o arquivo terá ao ser baixado
+
+    this.http.get(pdfUrl, { responseType: 'blob' }).subscribe((fileBlob) => {
+      const blob = new Blob([fileBlob], { type: 'application/pdf' });
+      const downloadURL = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = downloadURL;
+      link.download = pdfName;
+      link.click();
+    });
+  }
 }
